0f5421ca2e014ee3acb086308b2975c5240e1313
Ali Safi <ali800safi@gmail.com>
2021-05-22 18:30:17 +0430

    Write User Script

---
+++ Assets/Scripts/Users.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Users : MonoBehaviour
+{
+    public List<Card> cardsObject = new List<Card>();
+
+    private void Start()
+    {
+        InitCardsObject();
+    }
+
+    void InitCardsObject()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 13; j++)
+            {
+                Card.CardGroup cardGroup = (Card.CardGroup)i;
+                cardsObject.Add(new Card(j, cardGroup));
+                print($"{cardsObject[i]}");
+            }
+        }
+    }
+
+}
+
+[System.Serializable]
+public class User
+{
+    public int id;
+    public string userName;
+    public List<Card> currentCards = new List<Card>();
+    
+}
+[System.Serializable]
+public class Card
+{
+    public enum CardGroup
+    {
+        Heart=0,Diamond=1,Spade=2,Club=3
+    }
+    public int number;
+    public CardGroup cardGroup;
+    public Sprite spr;
+    public Card(int number,CardGroup cardGroup)
+    {
+        this.number = number;this.cardGroup = cardGroup;
+    }
+}
